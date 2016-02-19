a4afce95bc816e5a59db98bba29c2d9c5112d3a1
Svetlana Kofman <skofman@microsoft.com>
2016-02-19 14:35:26 -0800

    Stage service skeleton

---
+++ src/StageManager/Controllers/StageController.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+namespace Stage.Manager.Controllers
+{
+    [Route("api/[controller]")]
+    public class StageController : Controller
+    {
+        // GET: api/stage
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/stage/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/stage
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/stage/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/stage/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/StageManager/Startup.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.IO;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using IServiceCollection = Microsoft.Extensions.DependencyInjection.IServiceCollection;
+
+namespace Stage.Manager
+{
+    /// <summary>
+    /// Helpful links: 
+    /// https://github.com/Microsoft/ApplicationInsights-aspnetcore/wiki/Getting-Started
+    /// https://docs.asp.net/en/latest/fundamentals/startup.html
+    /// </summary>
+    public class Startup
+    {
+        private const string LocalEnvironmentName = "Local";
+        public IConfigurationRoot Configuration { get; set; }
+
+        public Startup(IHostingEnvironment hostingEnvironment, IApplicationEnvironment applicationEnvironment)
+        {
+            // Setup configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(applicationEnvironment.ApplicationBasePath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile(Path.Combine("Config", $"config.{hostingEnvironment.EnvironmentName}.json") )
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+            if (hostingEnvironment.IsEnvironment(LocalEnvironmentName))
+            {
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+        }
+       
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplicationInsightsTelemetry(Configuration);
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder applicationBuilder, IHostingEnvironment hostingEnvironment, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+
+            // Debug settings for local runs and for dev deployment
+            if (hostingEnvironment.IsEnvironment(LocalEnvironmentName) || hostingEnvironment.IsDevelopment())
+            {
+                loggerFactory.AddDebug();
+                applicationBuilder.UseDeveloperExceptionPage();
+            }
+
+            applicationBuilder.UseIISPlatformHandler();
+
+            // Add Application Insights monitoring to the request pipeline as a very first middleware.
+            applicationBuilder.UseApplicationInsightsRequestTelemetry();
+
+            // Add Application Insights exceptions handling to the request pipeline.
+            // Exception middleware should be added after error page and any other error handling middleware
+            applicationBuilder.UseApplicationInsightsExceptionTelemetry();
+
+            applicationBuilder.UseMvc();
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
