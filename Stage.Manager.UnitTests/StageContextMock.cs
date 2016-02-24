d7fcd43ca10af3b48e724438c320de80376012c7
Svetlana Kofman <skofman@microsoft.com>
2016-02-24 09:12:22 -0800

    Added UTs project

---
+++ Stage.Manager.UnitTests/StageContextMock.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+using Moq;
+using Stage.Database.Models;
+
+namespace Stage.Manager.UnitTests
+{
+    public class StageContextMock : Mock<StageContext>
+    {
+        private Dictionary<string, object> tables;
+
+
+        public StageContextMock()
+        {
+            MockTables();
+        }
+
+        // <summary>
+        /// Mocks all the DbSet{T} properties that represent tables and views.
+        /// </summary>
+        private void MockTables()
+        {
+            Type contextType = typeof(StageContext);
+            var dbSetProperties = contextType.GetProperties().Where(prop => (prop.PropertyType.IsGenericType) && prop.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>));
+            foreach (var prop in dbSetProperties)
+            {
+                var dbSetGenericType = prop.PropertyType.GetGenericArguments()[0];
+                Type listType = typeof(List<>).MakeGenericType(dbSetGenericType);
+                var listForFakeTable = Activator.CreateInstance(listType);
+                var parameter = Expression.Parameter(contextType);
+                var body = Expression.PropertyOrField(parameter, prop.Name);
+                var lambdaExpression = Expression.Lambda<Func<StageContext, object>>(body, parameter);
+                var method = typeof(StageContextMock).GetMethod("MockDbSet").MakeGenericMethod(dbSetGenericType);
+                this.Setup(lambdaExpression).Returns(method.Invoke(null, new[] { listForFakeTable }));
+                this.tables.Add(prop.Name, listForFakeTable);
+            }
+        }
+    }
+}
---
+++ Stage.Manager.UnitTests/StageControllerUnitTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
+using Stage.Manager.Controllers;
+using Xunit;
+
+namespace Stage.Manager.UnitTests
+{
+
+    /// <summary>
+    /// Helpful links: https://blogs.msdn.microsoft.com/webdev/2015/08/06/unit-testing-with-dnx-asp-net-5-projects/
+    /// </summary>
+    public class StageControllerUnitTests
+    {
+        private StageController _stageController;
+
+        public StageControllerUnitTests()
+        {
+            _stageController = new StageController((ILogger<StageController>)new DebugLogger(nameof(StageControllerUnitTests)), new StageContextMock().Object);
+        }
+
+        [Fact]
+        public void UniTest1()
+        {
+            Assert.True(true);
+        }
+    }
+
+}
