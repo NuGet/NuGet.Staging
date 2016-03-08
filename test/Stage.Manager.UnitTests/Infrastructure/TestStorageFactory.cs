bbba66182192b0704dc99e1a257cf645238aa99a
Svetlana Kofman <skofman@microsoft.com>
2016-03-08 08:55:51 -0800

    flat container UTs

---
+++ test/Stage.Manager.UnitTests/Infrastructure/TestStorageFactory.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using NuGet.Services.Metadata.Catalog.Persistence;
+
+namespace Stage.Manager.UnitTests
+{
+    public class TestStorageFactory
+        : StorageFactory
+    {
+        public Dictionary<string, Storage> CreatedStorages { get; }
+             
+        private readonly Func<string, Storage> _createStorage;
+
+        public TestStorageFactory()
+            : this(name => new MemoryStorage())
+        {
+        }
+
+        public TestStorageFactory(Func<string, Storage> createStorage)
+        {
+            _createStorage = createStorage;
+            BaseAddress = _createStorage(null).BaseAddress;
+            CreatedStorages = new Dictionary<string, Storage>();
+        }
+
+        public override Storage Create(string name = null)
+        {
+            var storage = _createStorage(name);
+            CreatedStorages[name] = storage;
+            return storage;
+        }
+    }
+}
