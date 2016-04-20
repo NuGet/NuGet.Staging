2077a0280cfb891dedc860b7752d7873ba4965ac
skofman <skofman@microsoft.com>
2016-04-20 10:19:12 -0700

    start

---
+++ src/NuGet.Services.Staging.BackgroundWorkers/ICommitStatusService.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using NuGet.Services.Staging.Database.Models;
+
+namespace NuGet.Services.Staging.BackgroundWorkers
+{
+    public interface ICommitStatusService
+    {
+        /// <summary>
+        /// Gets the latest commit of the stage.
+        /// </summary>
+        StageCommit GetCommit(string stageId);
+
+
+
+
+    }
+}
--- src/NuGet.Services.Staging.BackgroundWorkers/StageCommitWorker.cs
+++ src/NuGet.Services.Staging.BackgroundWorkers/StageCommitWorker.cs
@@ -12,10 +12,12 @@ namespace NuGet.Services.Staging.BackgroundWorkers
     {
         private readonly IMessageListener<PackageBatchPushData> _messageListener;
         private readonly ILogger<StageCommitWorker> _logger;
+        private readonly ICommitStatusService _commitStatusService;
 
         public bool IsActive => _messageListener.IsActive;
 
-        public StageCommitWorker(IMessageListener<PackageBatchPushData> messageListener, ILogger<StageCommitWorker> logger)
+        public StageCommitWorker(IMessageListener<PackageBatchPushData> messageListener, ICommitStatusService commitStatusService,
+            ILogger<StageCommitWorker> logger)
         {
             if (messageListener == null)
             {
@@ -27,8 +29,14 @@ namespace NuGet.Services.Staging.BackgroundWorkers
                 throw new ArgumentNullException(nameof(logger));
             }
 
+            if (commitStatusService == null)
+            {
+                throw new ArgumentNullException(nameof(commitStatusService));
+            }
+
             _messageListener = messageListener;
             _logger = logger;
+            _commitStatusService = commitStatusService;
         }
 
         public void Start()
@@ -43,6 +51,26 @@ namespace NuGet.Services.Staging.BackgroundWorkers
 
         internal async Task HandleBatchPushRequest(PackageBatchPushData pushData)
         {
+            /*
+             * Get commit information from the DB. If commit failed, discard the message.
+             * Create status dictionary: package to upload status. 
+             * Sort packages by dependency order
+             * for each package:
+             * 1. if status is "in progress": try to push. if fails on conflict, ignore the failure. Update the status to completed (in the DB).
+             * 2. if status is "pending": try to push. if fails (with retries), mark the status as failed and exit. if success, mark success (DB) and continue.
+             * 
+             * Once all packages are uploaded mark status as completed.
+             */
+
+
+
+
+
+
+
+
+
+
             _logger.LogInformation("Got message for {StageId}", pushData.StageId);
         }
     }
